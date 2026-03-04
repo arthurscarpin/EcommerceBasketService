691d106ea876530c6cc0c07cd412861eda6acc89
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2026-03-04 10:10:42 -0300

    feat: Implement Redis cache

--- src/main/java/dev/java/ecommerce/basketservice/service/ProductService.java
+++ src/main/java/dev/java/ecommerce/basketservice/service/ProductService.java
@@ -3,22 +3,29 @@ package dev.java.ecommerce.basketservice.service;
 import dev.java.ecommerce.basketservice.client.PlatziStoreClient;
 import dev.java.ecommerce.basketservice.client.response.PlatziProductResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
     private final PlatziStoreClient client;
 
+    @Cacheable(value = "products")
     public List<PlatziProductResponse> getAll() {
+        log.info("Getting all products");
         return client.getAll();
     }
 
-    public PlatziProductResponse getById(Long id) {
-        return client.getById(id);
+    @Cacheable(value = "product", key = "#productId")
+    public PlatziProductResponse getById(Long productId) {
+        log.info("Getting product with id: {}", productId);
+        return client.getById(productId);
     }
 
 }
